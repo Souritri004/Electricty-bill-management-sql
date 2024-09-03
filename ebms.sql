f0d82475fe5363d1d111c908d81bafa23d786c4d
Souritri Chatterjee <103194207+Souritri004@users.noreply.github.com>
2024-09-03 21:53:01 +0530

    ebms.sql

--- ebms.sql
+++ ebms.sql
@@ -1,8 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
---
--- Host: localhost    Database: ebms
--- ------------------------------------------------------
--- Server version	8.0.24
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
